9cb864fddea339021f0e995b5e766e1d397d0713
Himanshu Garg <himanshu8655@gmail.com>
2024-06-26 01:35:06 -0400

    backtracking and graph sums

--- leetcode/backtracking/CombinationSum.java
+++ leetcode/backtracking/CombinationSum.java
@@ -4,22 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CombinationSum {
-	  public List<List<Integer>> combinationSum(int[] candidates, int target) {
-	        List<List<Integer>> res = new ArrayList<>();
-	        dfs(0,0,new ArrayList<>(),candidates,target,res);
-	        return res;
-	    }
-	    void dfs(int index, int sum, List<Integer> combinations,int[] candidates, int target,List<List<Integer>> res){
-	        if(target == sum){
-	          res.add(new ArrayList<>(combinations));  
-	            return;
-	        }
-	        if(sum>target || index>=candidates.length)
-	            return;
-	        combinations.add(candidates[index]);
-	        dfs(index,sum + candidates[index], combinations, candidates, target, res);
-	        combinations.remove(combinations.size()-1);
-	        dfs(index+1,sum, combinations, candidates, target, res);
+	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+		List<List<Integer>> res = new ArrayList<>();
+		dfs(0, 0, new ArrayList<>(), candidates, target, res);
+		return res;
+	}
 
-	    }
+	void dfs(int index, int sum, List<Integer> combinations, int[] candidates, int target, List<List<Integer>> res) {
+		if (target == sum) {
+			res.add(new ArrayList<>(combinations));
+			return;
+		}
+		if (sum > target || index >= candidates.length)
+			return;
+		combinations.add(candidates[index]);
+		dfs(index, sum + candidates[index], combinations, candidates, target, res);
+		combinations.remove(combinations.size() - 1);
+		dfs(index + 1, sum, combinations, candidates, target, res);
+
+	}
 }
